4815ce8b7a4681149603de9d0ada9246c27b014e
gishan.akbaraly@gmail.com <gishan.akbaraly@gmail.com@97433ba1-6bf3-f306-2221-09d0dafe733b>
2014-12-29 21:46:02 +0000

    modif graph, ajout de deterministe directement dedans

--- trunk/src/tsp/PL.java
+++ trunk/src/tsp/PL.java
@@ -9,6 +9,9 @@ import java.util.Map.Entry;
 
 import javax.swing.text.StyledEditorKit.BoldAction;
 
+import CustomClass.PaireVertex;
+import Math.ExceptionMaths;
+import Math.Maths;
 import mvc.GraphOpt;
 /**
  * Classe Programme Linéaire
@@ -41,9 +44,37 @@ public class PL {
 		return null;
 	}
 	
-	public void initScenario(ArrayList<Scenario> s)
+	public void initScenario(ArrayList<Scenario> s, TSP tsp)
 	{
-		
+		for (Scenario scenario : s) {
+			Graph g = new Graph(tsp.getG().getVilles());
+			LinkedHashMap<PaireVertex, Double> tmp = new LinkedHashMap<>();
+			
+			for(int i=0; i<g.getNbVilles(); i++){
+				for(int j=0; j<g.getNbVilles();j++){
+					PaireVertex p = new PaireVertex(new Vertex(i),new Vertex(j));
+					if(tsp.getDeterminists().contains(p) || p.hasSameVertex()){
+						tmp.put(p, tsp.getG().getCouts().get(p));
+					}
+					else{
+						Double valeurXml = tsp.getG().getCouts().get(p);
+						Double ecartype = Maths.calculEcartType(tsp);
+						int nbvaleur = 3;
+						
+						Double min = valeurXml-ecartype;
+						Double max = valeurXml+ecartype;
+						try {
+							System.out.println(Maths.generateRandomCosts(valeurXml, nbvaleur, min, max));
+						} catch (ExceptionMaths e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+			g.setCouts(tmp);
+			scenario.setSolution(g);
+		}
 	}
 	
 	public Graph generateSolutionReference(Graph g)
